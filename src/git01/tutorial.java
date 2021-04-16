e20702a94a4bda5885ac73146ed510fe96027966
jorge <jorge@10.0.2.15>
2021-04-16 17:21:51 +0200

    Pirmera subida

---
+++ src/git01/tutorial.java
@@ -0,0 +1,10 @@
+package git01;
+
+import java.io.IOException;
+
+public class tutorial {
+	public static void main(String[] args) throws IOException {
+		System.out.println("CREACIÓN DE JAVA");
+	
+	}
+}
