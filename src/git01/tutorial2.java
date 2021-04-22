9125cb924124b5c9ce1c4c57f40d2c292692c69c
sandruski <sandrafriascu@gmail.com>
2021-04-22 04:22:18 +0200

    tutorial2

--- src/git01/tutorial2.java
+++ src/git01/tutorial2.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class tutorial2 {
 	public static void main(String[] args) throws IOException {
 	System.out.println("Nuevo java");
-	System.out.println("Nuevo java CAMBIO 3	");
+	
 	}
 }
