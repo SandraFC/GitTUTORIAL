4076ed0c13a9e8054926a68e57e979e0788b449a
SandraFC <82110357+SandraFC@users.noreply.github.com>
2021-04-22 05:07:10 +0200

    Update tutorial_2.java
    qdwd

--- src/git01/tutorial_2.java
+++ src/git01/tutorial_2.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 public class tutorial_2 {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Nuevo java 2");
-				System.out.println("Nuevo java 2");
 
+		System.out.println("Nuevo java 34");	
+
+		System.out.println("Nuevo java 2 ");
 	}
 
 }
